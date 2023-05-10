34f9cf757267736cd2c7d204a11b104d5290381a
BelalKhalifa <belalkhalifa88@gmail.com>
2023-05-10 19:38:11 +0200

    test5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,7 +70,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn aboutddddddurse with&nbsp;quizzes!
+            Learn adurse with&nbsp;quizzes!
           </p>
         </a>
 
