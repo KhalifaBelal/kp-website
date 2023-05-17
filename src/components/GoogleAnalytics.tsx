89a64a0d6625bd9a86919e6aa1feef34be1e0db1
BelalKhalifa <belalkhalifa88@gmail.com>
2023-05-17 20:54:57 +0200

    test

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -15,10 +15,10 @@ export default function GoogleAnalytics({
   const searchParams = useSearchParams();
 
   useEffect(() => {
-    const url = pathname + searchParams.toString();
+    const url = pathname
 
     pageview(GA_MEASUREMENT_ID, url);
-  }, [pathname, searchParams, GA_MEASUREMENT_ID]);
+  }, [pathname, GA_MEASUREMENT_ID]);
 
   return (
     <>
