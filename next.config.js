50aa21f60f4073acb628339aabbda209b93e0d7f
BelalKhalifa <belalkhalifa88@gmail.com>
2023-05-19 01:38:23 +0200

    test

--- next.config.js
+++ next.config.js
@@ -1,50 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+  output: "export",
   swcMinify: true,
+  images: { unoptimized: true },
   trailingSlash: true,
   compress: true,
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=604800",
-          },
-          {
-            key: "X-Frame-Options",
-            value: "SAMEORIGIN",
-          },
-          {
-            key: "X-XSS-Protection",
-            value: "1; mode=block",
-          },
-          {
-            key: "X-Content-Type-Options",
-            value: "nosniff",
-          },
-          {
-            key: "Referrer-Policy",
-            value: "same-origin",
-          },
-          {
-            key: "X-Permitted-Cross-Domain-Policies",
-            value: "none",
-          },
-          {
-            key: "X-DNS-Prefetch-Control",
-            value: "off",
-          },
-        ],
-      },
-    ];
-  },
 };
 
-module.exports = {
-  ...nextConfig,
-};
+module.exports = nextConfig;
