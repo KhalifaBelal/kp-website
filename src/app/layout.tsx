9207dc4f78a58c0cf27e39f309cf861ad2897098
BelalKhalifa <belalkhalifa88@gmail.com>
2023-05-23 12:35:15 +0200

    preload fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
 import dynamic from "next/dynamic";
 import Navbar from "@/components/Navbar";
+import Head from "next/head";
 const CookieBanner = dynamic(() => import("@/components/CookieBanner"), {
   loading: () => <div>Loading...</div>,
   ssr: false,
@@ -29,6 +30,16 @@ export default function RootLayout({
       className={cn("bg-white text-zinc-900 antialiased", montserrat.className)}
     >
       <GoogleAnalytics GA_MEASUREMENT_ID="G-KG3VJEPD33" />
+      <Head>
+        <link
+          rel="preload"
+          href="out/_next/static/media/1060bab20f18b5c2-s.p.woff2"
+          type="font"
+          as="font/woff2"
+          crossOrigin="anonymous"
+          data-next-font="size-adjust"
+        />
+      </Head>
       <body className="min-h-screen bg-zinc-900 antialiased">
         <CookieBanner />
         <Navbar />
