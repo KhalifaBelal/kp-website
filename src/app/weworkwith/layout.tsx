2a3319ed29c12a07cd3a72f1a7a30511ec075d35
BelalKhalifa <belalkhalifa88@gmail.com>
2023-05-25 21:00:07 +0200

    test

--- src/app/weworkwith/layout.tsx
+++ src/app/weworkwith/layout.tsx
@@ -1,4 +1,5 @@
 import SubNavbar from "@/components/SubNavbar";
+import KPSection from "@/components/ui/KPSection";
 
 import type { Metadata } from "next";
 
@@ -13,9 +14,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <KPSection>
       <SubNavbar />
       {children}
-    </>
+    </KPSection>
   );
 }
--- src/components/sections/investors/InvestIntro.tsx
+++ src/components/sections/investors/InvestIntro.tsx
@@ -1,13 +1,26 @@
+"use client";
+
 import KPHeading from "@/components/ui/KPHeading";
 import KPParagraph from "@/components/ui/KPParagraph";
+import { AnimatedParagraph } from "@/components/ui/AnimatedParagraph";
 import KPRow from "@/components/ui/KPRow";
 import KPSection from "@/components/ui/KPSection";
+import { motion } from "framer-motion";
+import { staggerContainer } from "@/utils/motion";
 
 function InvestIntro() {
   return (
     <KPSection>
       <KPRow variant="intro" flexDirection="col">
-        <KPParagraph>We work with investors</KPParagraph>
+        <motion.div
+          variants={staggerContainer(0.1, 1)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: false, amount: 0.25 }}
+        >
+          <AnimatedParagraph title="We work with investors"></AnimatedParagraph>
+        </motion.div>
+
         <KPHeading size="sm" fontWeight="normal">
           <span className="font-semibold">Unlocking Potential:</span> Invest in
           the Khalifa Partners Investment Fund Are you ready to be part of a
