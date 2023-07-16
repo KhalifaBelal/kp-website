e9f8fd047f60ca64e60e722078e1e8248c1399cf
BelalKhalifa <belalkhalifa88@gmail.com>
2023-07-16 12:41:18 +0200

    new texts

--- src/components/sections/kp-investors/landingPage/InvestorsPortalTeaser.tsx
+++ src/components/sections/kp-investors/landingPage/InvestorsPortalTeaser.tsx
@@ -8,13 +8,15 @@ function InvestorsPortalTeaser() {
   return (
     <div className="w-full py-10 bg-gradient-to-r to-kp-red via-red-900 from-zinc-800">
       <KPRow flexDirection="col" alignItems="start">
-        <KPHeading className="text-white" size="lg">COMING SOON</KPHeading>
-        <KPHeading className="text-white">Investors Portal</KPHeading>
+        <KPHeading className="text-white" fontWeight="normal">
+          Coming Soon: Elevate Your Investment Experience!
+        </KPHeading>
         <KPParagraph className="text-zinc-300">
-          Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-          nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
-          sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
-          rebum.
+          Exciting news! Our Investor Portal is coming soon, designed to
+          revolutionize your investment journey. View your investments, track
+          profits, and enjoy a seamless experience – all at your fingertips.
+          Stay tuned for the launch and gear up for a more empowered approach to
+          financial success. Get ready to elevate your investment game!
         </KPParagraph>
       </KPRow>
     </div>
