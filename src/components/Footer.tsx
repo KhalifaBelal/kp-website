737ab04618e45fe664702fc294903d71c9e6f2fd
KhalifaBelal <130314671+KhalifaBelal@users.noreply.github.com>
2024-02-26 11:19:32 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,7 +20,7 @@ export default function Footer() {
         />
         <KPParagraph className="text-zinc-300 text-center">
           <span className="font-bold">Khalifa Partners®</span>
-          <br />Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.
+          <br />We diversify our Portfolio by building companies with outstanding personalities.
         </KPParagraph>
         <div className="flex flex-col md:flex-row items-center gap-2 flex-1">
           <Link className={kpButtonVariants({ variant: "link" })} href="https://staging.khalifapartners.com/">
