8c2507be977c7352e0c3d227c3ab43d8d194a9e9
KhalifaBelal <belalkhalifa88@gmail.com>
2024-07-30 19:18:20 +0200

    fix

--- src/components/sections/kp-entrepreneurs/landingPage/OurPortfolio.tsx
+++ src/components/sections/kp-entrepreneurs/landingPage/OurPortfolio.tsx
@@ -51,26 +51,6 @@ const PORTFOLIO_DATA: {
   website?: string;
 }[] = [
     {
-      name: "Salespects",
-      description: "Salespects is a SaaS solution and the first CRM system exclusively for financial advisors in Germany. Targeting a market of over 300,000 potential clients, Salespects has been in development for over two years and is set to launch in Q4 of 2024.",
-      twColor: "",
-      companyState: "Currently Building",
-      image: {
-        src: "/logos/salespects-logo.png",
-        alt: "salespects-logo"
-      }
-    },
-    {
-      name: "MyPerfectStay",
-      description: "MyPerfectStay is an AI-based mobile application designed to tailor the perfect vacation for tourists around the world. Featuring over 300,000 activities and a unique AI algorithm, this innovative app allows tourists to not only plan but also book their activities at their preferred destination. MyPerfectStay will launch in Q4 of 2024.",
-      twColor: "",
-      companyState: "Currently Building",
-      image: {
-        src: "/logos/myperfectstay-logo.png",
-        alt: "myperfectstay-logo"
-      }
-    },
-    {
       name: "Clartus",
       description: "Clartus is our business consultancy dedicated to helping 7-8 figure companies generate more growth through data-driven marketing strategies. We specialize in Meta Ads, Google Ads, SEO, UX Design, and Software Development. Based in the UAE, Clartus serves clients in the UAE, Germany, and Egypt.",
       twColor: "",
@@ -95,10 +75,30 @@ const PORTFOLIO_DATA: {
       }
     },
     {
+      name: "Salespects",
+      description: "Salespects is a SaaS solution and the first CRM system exclusively for financial advisors in Germany. Targeting a market of over 300,000 potential clients, Salespects has been in development for over two years and will launch in 2024.",
+      twColor: "",
+      companyState: "Currently Building",
+      image: {
+        src: "/logos/salespects-logo.png",
+        alt: "salespects-logo"
+      }
+    },
+    {
+      name: "MyPerfectStay",
+      description: "MyPerfectStay is an AI-based mobile application designed to tailor the perfect vacation for tourists around the world. Featuring over 300,000 activities and a unique AI algorithm, this innovative app allows tourists to not only plan but also book their activities at their preferred destination. MyPerfectStay will launch in 2025.",
+      twColor: "",
+      companyState: "Currently Building",
+      image: {
+        src: "/logos/myperfectstay-logo.png",
+        alt: "myperfectstay-logo"
+      }
+    },
+    {
       name: "SmartinCulture",
       description: "SmartinCulture helps 8-figure entrepreneurs and established companies from the DACH Region (Germany, Austria, and Switzerland) expand into the UAE and Saudi markets. We open doors to new customers and partners, set up the local business with the right legal foundation, and assist with operational tasks such as Sales, Marketing, and HR.",
       twColor: "",
-      companyState: "Launched",
+      companyState: "Currently Building",
       // website: "http://smartinculture.com",
       cta: "smartinculture.com",
       image: {
@@ -110,7 +110,7 @@ const PORTFOLIO_DATA: {
       name: "EmergingExperts",
       description: "EmergingExperts is our recruiting company based in Egypt, dedicated to attracting the top 1% of talent in the fields of Software Development and Graphic Design. We connect this elite talent with work opportunities that are not available in the national market.",
       twColor: "",
-      companyState: "Launched",
+      companyState: "Currently Building",
       // website: "http://emergingexperts.com",
       cta: "emergingexperts.com",
       image: {
@@ -122,12 +122,12 @@ const PORTFOLIO_DATA: {
       name: "CodeNatura",
       description: "CodeNatura is an international e-commerce brand focusing on natural supplements like Himalayan Shilajit. With thousands of loyal customers, we are committed to providing the best product quality on the market.",
       twColor: "",
-      companyState: "Launched",
+      companyState: "Currently Building",
       // website: "http://codenatura.com",
       cta: "http://codenatura.com",
       image: {
         src: "/logos/lock.png",
         alt: "codenatura-logo"
       }
-    }
+    },
   ]
