2c947d853eab28b68385babdeab533d3b016d3f4
KhalifaBelal <belalkhalifa88@gmail.com>
2024-07-30 18:48:38 +0200

    eslint fixes

--- src/components/sections/kp-entrepreneurs/landingPage/Concept.tsx
+++ src/components/sections/kp-entrepreneurs/landingPage/Concept.tsx
@@ -17,7 +17,7 @@ function Concept() {
           <KPParagraph>
             Khalifa Partners is an international business incubator dedicated to building successful businesses around the globe. By focusing on the top 1% of talent, we foster a culture of excellence to create enduring companies that make a lasting impact.
             <br /><br />
-            Inspired by our Egyptian heritage, driven by German precision, and motivated by the UAE's economic vision, we merge diverse strengths to build a robust portfolio of ventures with passionate, talented individuals.
+            Inspired by our Egyptian heritage, driven by German precision, and motivated by the UAE&apos;s economic vision, we merge diverse strengths to build a robust portfolio of ventures with passionate, talented individuals.
           </KPParagraph>
         </KPRow>
         <div className="relative w-full aspect-square">
--- src/components/sections/kp-entrepreneurs/landingPage/Founders.tsx
+++ src/components/sections/kp-entrepreneurs/landingPage/Founders.tsx
@@ -29,7 +29,7 @@ function Founders() {
               I am the guy connecting the dots. I link investors with ideas, validate business concepts, build the teams that execute those ideas, and handle investor relations. As the strategic head behind Khalifa Partners, I ensure that each venture is meticulously planned and executed to stand the test of time.
             </KPParagraph>
             <div className='flex flex-col items-start justify-center gap-2'>
-              <span className='text-sm text-kp-red font-medium italic'>let's connect</span>
+              <span className='text-sm text-kp-red font-medium italic'>let&apos;s connect</span>
               <div className='flex items-center justify-center gap-2'>
                 <Link href={"https://www.linkedin.com/in/basel-khalifa-06b6ab1a0/"} className={cn(kpButtonVariants(), 'aspect-square w-auto h-auto p-1 bg-zinc-700')}><Image src={"/linkedin-icon.png"} alt='linkedin-icon' width={48} height={48} /></Link>
                 <Link href={"https://www.instagram.com/basel.khalifa/"} className={cn(kpButtonVariants(), 'aspect-square w-auto h-auto p-1 bg-zinc-700')}><Image src={"/instagram-icon.png"} alt='instagram-icon' width={48} height={48} /></Link>
@@ -50,7 +50,7 @@ function Founders() {
               I am the creative and technical head of Khalifa Partners, managing the technical infrastructure of our projects and overseeing the creative aspects, including product and brand design. My role ensures that our ventures are not only functional but also innovative and visually compelling.
             </KPParagraph>
             <div className='flex flex-col items-start justify-center gap-2'>
-              <span className='text-sm text-kp-red font-medium italic'>let's connect</span>
+              <span className='text-sm text-kp-red font-medium italic'>let&apos;s connect</span>
               <div className='flex items-center justify-center gap-2'>
                 <Link href={"https://www.linkedin.com/in/belal-khalifa-b22553307/"} className={cn(kpButtonVariants(), 'aspect-square w-auto h-auto p-1 bg-zinc-700')}><Image src={"/linkedin-icon.png"} alt='linkedin-icon' width={48} height={48} /></Link>
                 <Link href={"https://www.instagram.com/belal.emad.khalifa/"} className={cn(kpButtonVariants(), 'aspect-square w-auto h-auto p-1 bg-zinc-700')}><Image src={"/instagram-icon.png"} alt='instagram-icon' width={48} height={48} /></Link>
