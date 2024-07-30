52dc041d0f405739a40739038bb66ca31d0d458b
KhalifaBelal <belalkhalifa88@gmail.com>
2024-07-30 19:45:08 +0200

    spacing

--- src/components/sections/kp-entrepreneurs/landingPage/Philosophy.tsx
+++ src/components/sections/kp-entrepreneurs/landingPage/Philosophy.tsx
@@ -8,7 +8,7 @@ function Philosophy() {
   return (
     <KPSection id="philosophy" className="gap-5 p-0 bg-black">
       <KPRow flexDirection="col" alignItems="start" className="flex-1 lg:p-16">
-        <KPHeading>Our Philosophy</KPHeading>
+        <KPHeading className='mt-16 lg:mt-0'>Our Philosophy</KPHeading>
         <KPParagraph>
           At Khalifa Partners, our business philosophy is deeply rooted in the timeless wisdom and engineering marvels of the ancient Egyptian pyramids. Just as the pyramids were meticulously designed and built to stand the test of time, we believe in constructing enduring companies that leave a lasting impact. The pyramid is not only our logo but also symbolizes our founders&apos; Egyptian heritage and serves as a powerful metaphor for our approach to business. Like the pyramid builders of old, we meticulously plan, strategize, and execute each step to build successful companies. Our commitment to excellence is driven by our dedication to putting people first, ensuring that the businesses we build are not only successful but also enduring.
         </KPParagraph>
